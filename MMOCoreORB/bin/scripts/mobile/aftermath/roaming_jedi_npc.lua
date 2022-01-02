0a549e9351ae9e5ede2bc1dcad4129729321c993
Alpha <jamesmorris9@hotmail.com>
2022-01-02 11:54:00 +0000

    [updated] attack types to aftermath npcs

--- MMOCoreORB/bin/scripts/mobile/aftermath/roaming_jedi_npc.lua
+++ MMOCoreORB/bin/scripts/mobile/aftermath/roaming_jedi_npc.lua
@@ -55,9 +55,17 @@ roaming_jedi_npc = Creature:new {
 			lootChance = 10000000
 		}
 	},
-	weapons = {"light_jedi_weapons"},
+	
+	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
+	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
+	primaryWeapon = "light_jedi_weapons",
+	secondaryWeapon = "unarmed",
 	conversationTemplate = "",
-	attacks = merge(lightsabermaster)
+	
+	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
+	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
+	primaryAttacks = merge(brawlernovice,lightsabermaster),
+	secondaryAttacks = { }
 }
 
 CreatureTemplates:addCreatureTemplate(roaming_jedi_npc, "roaming_jedi_npc")
