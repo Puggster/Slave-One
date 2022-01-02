a873672d574ce51aff4af281439c10b5dd7f5a66
Alpha <jamesmorris9@hotmail.com>
2022-01-02 15:37:22 +0000

    [added] mob type to death watch commando

--- MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/death_watch_commando.lua
+++ MMOCoreORB/bin/scripts/mobile/dungeon/death_watch_bunker/death_watch_commando.lua
@@ -1,5 +1,6 @@
 death_watch_commando = Creature:new {
 	objectName = "@mob/creature_names:death_watch_elite_commando",
+	mobType = MOB_NPC,
 	randomNameType = NAME_GENERIC,
 	randomNameTag = true,
 	socialGroup = "death_watch",
