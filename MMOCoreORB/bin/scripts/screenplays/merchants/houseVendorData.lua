f674e7ca778ae6944800dd8b8936e0d815ca7ed7
Alpha <jamesmorris9@hotmail.com>
2022-01-09 17:08:44 +0000

    [added] Screenplays - POI, Merchants

---
+++ MMOCoreORB/bin/scripts/screenplays/merchants/houseVendorData.lua
@@ -0,0 +1,20 @@
+HouseVendorLogic = VendorLogic:new {
+	scriptName = "HouseVendorLogic",
+	currencies = {
+	--For Tokens: Displayed Name, full template string (without shared_), if applicable: ScreenPlayData string, ScreenPlayData key
+		{currency = "credits"},
+	},
+	--Displayed Name, full template string (without shared_), cost in {}, use the same structure as currencies
+
+	merchandise = {
+	{name ="Mustafarian Underground Bunker Schematic", template = "object/tangible/loot/loot_schematic/musty_house_loot_schem.iff", cost = {10000000,}},
+        {name ="Vehicle Garage Schematic", template = "object/tangible/loot/loot_schematic/vehicle_house_loot_schem.iff", cost = {10000000,}},
+        {name ="Bespin Cloud House Schematic", template = "object/tangible/loot/loot_schematic/bespin_house_loot_schem.iff", cost = {10000000,}},
+				{name ="VIP Bunker Schematic", template = "object/tangible/loot/loot_schematic/vipbunker_house_loot_schem.iff", cost = {4000000,}},
+				{name ="YT-1300 Light Freighter House Schematic", template = "object/tangible/loot/loot_schematic/yt1300_house_loot_schem.iff", cost = {10000000,}},
+				{name ="Jabba's Sail Barge House Schematic(Tat/Lok only)", template = "object/tangible/loot/loot_schematic/jabbas_house_loot_schem.iff", cost = {10000000,}},
+				{name ="A Crate of Resources(1000)", template = "object/tangible/veteran_reward/resource_supply_crate.iff", cost = {200000,}},
+	},
+}
+
+registerScreenPlay("HouseVendorLogic", false)
---
+++ MMOCoreORB/bin/scripts/screenplays/merchants/restussRepData.lua
@@ -0,0 +1,29 @@
+RestussRepVendorLogic = VendorLogic:new {
+	scriptName = "RestussRepVendorLogic",
+	currencies = {
+	--For Tokens: Displayed Name, full template string (without shared_), if applicable: ScreenPlayData string, ScreenPlayData key
+		{currency = "token", name = "Restuss Commendations", template = "object/tangible/loot/misc/event_token_restuss.iff", ScreenPlayDataString = "vendorToken", ScreenPlayDataKey = "event_token"},
+	},
+	--Displayed Name, full template string (without shared_), cost in {}, use the same structure as currencies
+	merchandise = {
+{name = "Large Block Turret", template = "object/tangible/deed/faction_perk/turret/block_lg_deed.iff", cost = {30}},
+{name = "Medium Block Turret", template = "object/tangible/deed/faction_perk/turret/block_med_deed.iff", cost = {20}},
+{name = "Small Block Turret", template = "object/tangible/deed/faction_perk/turret/block_sm_deed.iff", cost = {10}},
+{name = "Large Dish Turret", template = "object/tangible/deed/faction_perk/turret/dish_lg_deed.iff", cost = {30}},
+{name = "Small Dish Turret", template = "object/tangible/deed/faction_perk/turret/dish_sm_deed.iff", cost = {10}},
+{name = "Large Tower Turret", template = "object/tangible/deed/faction_perk/turret/tower_lg_deed.iff", cost = {30}},
+{name = "Medium Tower Turret", template = "object/tangible/deed/faction_perk/turret/tower_med_deed.iff", cost = {20}},
+{name = "Small Tower Turret", template = "object/tangible/deed/faction_perk/turret/tower_sm_deed.iff", cost = {10}},
+{name = "DC-15 Carbine Schematic", template = "object/tangible/loot/loot_schematic/dc15_carbine_schematic.iff", cost = {30}},
+{name = "DC-15A Rifle Schematic", template = "object/tangible/loot/loot_schematic/dc15_rifle_schematic.iff", cost = {30}},
+{name = "Disruptor Rifle Schematic", template = "object/tangible/loot/loot_schematic/mustafar_disruptor_rifle_schematic.iff", cost = {30}},
+{name = "E5 Carbine Schematic", template = "object/tangible/loot/loot_schematic/e5_carbine_schematic.iff", cost = {30}},
+{name = "Magnaguard Staff Schematic", template = "object/tangible/loot/loot_schematic/polearm_magnaguard_schematic.iff", cost = {30}},
+{name = "Jedi Holocron", template = "object/tangible/jedi/jedi_holocron_light.iff", cost = {25}},
+{name = "Sith Holocron", template = "object/tangible/jedi/jedi_holocron_dark.iff", cost = {25}},
+{name = "A Crate of Resources(1000)", template = "object/tangible/veteran_reward/resource_supply_crate.iff", cost = {50}},
+
+	},
+}
+
+registerScreenPlay("RestussRepVendorLogic", false)
