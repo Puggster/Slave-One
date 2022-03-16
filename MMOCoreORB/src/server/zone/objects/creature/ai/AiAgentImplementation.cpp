8c7e14ecaa7af2512c8e4f221ed8c12f9ab16520
hakry <hakry@swgemu.com>
2022-03-16 19:27:59 -0400

    [fixed] Ai being able to attack players with a gcw faction onleave [fixed] Ai attacking other dead AI [fixed] Ai deathblowing players
    Change-Id: Id3c01f9e9126fd4f65c107e50af2a5621cd1d84e

--- MMOCoreORB/src/server/zone/objects/creature/ai/AiAgentImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/ai/AiAgentImplementation.cpp
@@ -1135,7 +1135,7 @@ bool AiAgentImplementation::validateTarget(SceneObject* target) {
 			return false;
 		}
 
-		if (!targetCreO->isAttackableBy(asAiAgent())) {
+		if (targetCreO->isIncapacitated() || !targetCreO->isAttackableBy(asAiAgent())) {
 			return false;
 		}
 	} else if (target->isTangibleObject()) {
@@ -3588,7 +3588,7 @@ bool AiAgentImplementation::isAttackableBy(CreatureObject* creature) {
 	if (pvpStatusBitmask == 0 || !(pvpStatusBitmask & CreatureFlag::ATTACKABLE))
 		return false;
 
-	if (movementState == AiAgent::LEASHING)
+	if (movementState == AiAgent::LEASHING || isDead() || isIncapacitated())
 		return false;
 
 	// Handle Pets - Check against owner if they are the last commander
