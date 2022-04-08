876b20665815269f25509ede161b4c79a300a99f
hakry <hakry@swgemu.com>
2022-04-08 19:12:08 -0400

    [fixed] Typo in Jedi robe templates

--- MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_jedi_light_s02.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_jedi_light_s02.lua
@@ -43,33 +43,40 @@
 
 object_tangible_wearables_robe_robe_jedi_light_s02 = object_tangible_wearables_robe_shared_robe_jedi_light_s02:new {
 	playerRaces = { "object/creature/player/bothan_male.iff",
-				"object/creature/player/bothan_female.iff",
-				"object/creature/player/human_male.iff",
-				"object/creature/player/human_female.iff",
-				"object/creature/player/ithorian_male.iff",
-				"object/creature/player/ithorian_female.iff",
-				"object/creature/player/moncal_male.iff",
-				"object/creature/player/moncal_female.iff",
-				"object/creature/player/rodian_male.iff",
-				"object/creature/player/rodian_female.iff",
-				"object/creature/player/sullustan_male.iff",
-				"object/creature/player/sullustan_female.iff",
-				"object/creature/player/trandoshan_male.iff",
-				"object/creature/player/trandoshan_female.iff",
-				"object/creature/player/twilek_male.iff",
-				"object/creature/player/twilek_female.iff",
-				"object/creature/player/wookiee_male.iff",
-				"object/creature/player/wookiee_female.iff",
-				"object/creature/player/zabrak_male.iff",
-				"object/creature/player/zabrak_female.iff" },
+		"object/creature/player/bothan_female.iff",
+		"object/creature/player/human_male.iff",
+		"object/creature/player/human_female.iff",
+		"object/creature/player/ithorian_male.iff",
+		"object/creature/player/ithorian_female.iff",
+		"object/creature/player/moncal_male.iff",
+		"object/creature/player/moncal_female.iff",
+		"object/creature/player/rodian_male.iff",
+		"object/creature/player/rodian_female.iff",
+		"object/creature/player/sullustan_male.iff",
+		"object/creature/player/sullustan_female.iff",
+		"object/creature/player/trandoshan_male.iff",
+		"object/creature/player/trandoshan_female.iff",
+		"object/creature/player/twilek_male.iff",
+		"object/creature/player/twilek_female.iff",
+		"object/creature/player/wookiee_male.iff",
+		"object/creature/player/wookiee_female.iff",
+		"object/creature/player/zabrak_male.iff",
+		"object/creature/player/zabrak_female.iff" },
 
-				skillMods = {
-				},
+	maxCondition = 100,
+	volume = 1,
+	jediRobe = true,
 
-				noTrade = 1,
+	skillMods = {
+		{"jedi_force_power_max", 250},
+		{"jedi_force_power_regen", 10}
+	},
 
-				objectMenuComponent = "RobeObjectMenuComponent",
-				attributeListComponent = "JediRobeAttributeListComponent",
+	noTrade = 1,
+
+	objectMenuComponent = "RobeObjectMenuComponent",
+	certificationsRequired = {"force_rank_light_rank_01"},
+	attributeListComponent = "JediRobeAttributeListComponent",
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_robe_robe_jedi_light_s02, "object/tangible/wearables/robe/robe_jedi_light_s02.iff")
