6e740ace3acf0983890a453f86f0885dc3af4933
hakry <hakry@swgemu.com>
2022-04-29 14:39:11 -0400

    [fixed] Armor resists to properly decay based on damage mitigated by armor - Mantis #7687 [fixed] turrets examine to show proper data and armor decimals"
    Change-Id: Idc4f23649b408668267c655bd3622cc5eb3566e4

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -1631,7 +1631,11 @@ int CombatManager::applyDamage(CreatureObject* attacker, WeaponObject* weapon, T
 		if (armorReduction >= 0)
 			damage *= getArmorPiercing(defender, armorPiercing);
 
-		damage *= (1.f - (armorReduction / 100.f));
+		if (armorReduction > 0) {
+			damage *= (1.f - (armorReduction / 100.f));
+
+			defender->addUnmitigatedDamage(damage);
+		}
 	}
 
 	defender->inflictDamage(attacker, 0, damage, true, xpType, true, true);
@@ -2370,8 +2374,12 @@ int CombatManager::getArmorReduction(TangibleObject* attacker, WeaponObject* wea
 		if (armorReduction >= 0)
 			damage *= getArmorPiercing(cast<AiAgent*>(defender), armorPiercing);
 
-		if (armorReduction > 0)
+		if (armorReduction > 0) {
 			damage *= (1.f - (armorReduction / 100.f));
+
+			if (!defender->isPet())
+				defender->addUnmitigatedDamage(damage);
+		}
 
 		return damage;
 	} else if (defender->isVehicleObject()) {
