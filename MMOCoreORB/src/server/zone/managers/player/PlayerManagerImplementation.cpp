8c844488ff87129dd97418ee7b0f48118e62e8b1
hakry <hakry@swgemu.com>
2022-05-06 21:26:38 -0400

    [fixed] Event mobs will no longer give trapping or creature handling XP - Mantis #8441
    Change-Id: I4ff245177fa45b26b688ad95c9adab0bedebd822

--- MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -2017,6 +2017,9 @@ void PlayerManagerImplementation::disseminateExperience(TangibleObject* destruct
 				continue;
 			}
 
+			if (destructedObject->isAiAgent() && destructedObject->asAiAgent()->isEventMob())
+				continue;
+
 			CreatureObject* attackerCreo = attacker->asCreatureObject();
 
 			if (attackerCreo == nullptr) {
@@ -2039,6 +2042,7 @@ void PlayerManagerImplementation::disseminateExperience(TangibleObject* destruct
 			Locker crossLocker(owner, destructedObject);
 
 			PlayerObject* ownerGhost = owner->getPlayerObject();
+
 			if (ownerGhost == nullptr || !owner->hasSkill("outdoors_creaturehandler_novice") || !destructedObject->isInRange(owner, 80)) {
 				continue;
 			}
