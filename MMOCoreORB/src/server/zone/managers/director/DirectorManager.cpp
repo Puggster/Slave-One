swgemu/Core3

[added] LuaEngine log level able to be set via config
Change-Id: If4924acfc0ddf9219822c7d598648fc6c9093c25

MMOCoreORB/src/server/zone/managers/director/DirectorManager.cpp
@@ -366,7 +366,7 @@ void DirectorManager::initializeLuaEngine(Lua* luaEngine) {
 	if (DEBUG_MODE) {
 		luaEngine->setLogLevel(Logger::DEBUG);
 	} else {
-		luaEngine->setLogLevel(Logger::INFO);
+		luaEngine->setLogLevel(ConfigManager::instance()->getLogLevel("Core3.LuaEngine.LogLevel", Logger::LOG));
 		luaEngine->setLogToConsole(false);
 	}
 

Puggster/Slave-One

MMOCoreORB/src/server/zone/managers/director/DirectorManager.cpp
@@ -367,7 +367,7 @@ void DirectorManager::initializeLuaEngine(Lua* luaEngine) {
 	if (DEBUG_MODE) {
 		luaEngine->setLogLevel(Logger::DEBUG);
 	} else {
-		luaEngine->setLogLevel(Logger::INFO);
+		luaEngine->setLogLevel(ConfigManager::instance()->getLogLevel("Core3.LuaEngine.LogLevel", Logger::ERROR));
 		luaEngine->setLogToConsole(false);
 	}
 