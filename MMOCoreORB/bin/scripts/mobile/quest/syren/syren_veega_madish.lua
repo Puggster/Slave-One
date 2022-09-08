swgemu/Core3

[Added] Secrets of the Syren quest line for rebels.
Quest items are not given and POI NPC's are not spawned yet.

Change-Id: I3975c69cd389ee993bacd56bcf0e72d107bf41b2

MMOCoreORB/bin/scripts/mobile/quest/syren/syren_veega_madish.lua
@@ -1,0 +1,36 @@
+syren_veega_madish = Creature:new {
+	objectName = "",
+	customName = "Veega Madish",
+	socialGroup = "rebel",
+	faction = "rebel",
+	level = 15,
+	chanceHit = 0.31,
+	damageMin = 160,
+	damageMax = 170,
+	baseXp = 831,
+	baseHAM = 2400,
+	baseHAMmax = 3000,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = PACK + STALKER,
+	optionsBitmask = AIENABLED + CONVERSABLE,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_rebel_colonel_bothan_male.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "rebelVeegaMadishConvoTemplate",
+	attacks = merge(pistoleermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(syren_veega_madish, "syren_veega_madish")

Puggster/Slave-One

MMOCoreORB/bin/scripts/mobile/quest/syren/syren_veega_madish.lua
@@ -1,0 +1,37 @@
+syren_veega_madish = Creature:new {
+	objectName = "",
+	customName = "Veega Madish",
+	socialGroup = "rebel",
+	faction = "rebel",
+	mobType = MOB_NPC,
+	level = 15,
+	chanceHit = 0.31,
+	damageMin = 160,
+	damageMax = 170,
+	baseXp = 831,
+	baseHAM = 2400,
+	baseHAMmax = 3000,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = NONE,
+	optionsBitmask = INVULNERABLE + CONVERSABLE,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/dressed_rebel_colonel_bothan_male.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "rebelVeegaMadishConvoTemplate",
+	attacks = merge(pistoleermaster,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(syren_veega_madish, "syren_veega_madish")