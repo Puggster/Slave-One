291e7077ca8bbf44e1c793623ca6cf8612430321
hakry <hakry@swgemu.com>
2022-12-19 22:55:06 -0500

    [updated] More planet regions [reduced] Minimum distance for lair spawn attempts
    Change-Id: Iafec6501114c9959489aa6772183d23b6939d429

--- MMOCoreORB/src/server/zone/managers/planet/PlanetManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/planet/PlanetManagerImplementation.cpp
@@ -1109,7 +1109,7 @@ void PlanetManagerImplementation::readRegionObject(LuaObject& regionObject) {
 	}
 
 #ifdef DEBUG_REGIONS
-	info(true) << "readRegion -- Name: " << name << "   COMPLETE";
+	info(true) << "FINISHED Loading Region -- Name: " << name;
 #endif // DEBUG_REGIONS
 }
 
