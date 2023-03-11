91eb3bae7d44d44638dd6bd94becc6afed988f76
hakry <hakry@swgemu.com>
2023-03-11 11:33:09 -0500

    [fixed] encumbrance on ubese armor bandoliers - Mantis #8813

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/ubese/armor_ubese_bandolier.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/ubese/armor_ubese_bandolier.lua
@@ -129,3 +129,38 @@ object_tangible_wearables_armor_ubese_armor_ubese_bandolier = object_tangible_we
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_armor_ubese_armor_ubese_bandolier, "object/tangible/wearables/armor/ubese/armor_ubese_bandolier.iff")
+				"object/mobile/vendor/zabrak_male.iff" },
+
+	-- Damage types in WeaponObject
+	vulnerability = NONE,
+
+	-- These are default Blue Frog stats
+	healthEncumbrance = 1,
+	actionEncumbrance = 1,
+	mindEncumbrance = 1,
+
+	-- LIGHT, MEDIUM, HEAVY
+	rating = LIGHT,
+
+	kinetic = 0,
+	energy = 0,
+	electricity = 0,
+	stun = 0,
+	blast = 0,
+	heat = 0,
+	cold = 0,
+	acid = 0,
+	lightSaber = 0,
+
+	numberExperimentalProperties = {1, 1, 1, 1},
+	experimentalProperties = {"XX", "XX", "XX", "XX"},
+	experimentalWeights = {1, 1, 1, 1},
+	experimentalGroupTitles = {"null", "null", "null", "exp_durability"},
+	experimentalSubGroupTitles = {"null", "null", "sockets", "hitpoints"},
+	experimentalMin = {0, 0, 0, 1000},
+	experimentalMax = {0, 0, 0, 1000},
+	experimentalPrecision = {0, 0, 0, 0},
+	experimentalCombineType = {0, 0, 4, 4},
+}
+
+ObjectTemplates:addTemplate(object_tangible_wearables_armor_ubese_armor_ubese_bandolier, "object/tangible/wearables/armor/ubese/armor_ubese_bandolier.iff")
