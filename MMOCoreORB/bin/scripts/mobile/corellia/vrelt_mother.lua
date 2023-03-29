swgemu/Core3

[fixed] creature templates missing an unarmed weapon - Part 2
Change-Id: I74a70f9d20d68140859670f7ba465292e67e45b8

MMOCoreORB/bin/scripts/mobile/corellia/vrelt_mother.lua
@@ -33,14 +33,14 @@ vrelt_mother = Creature:new {
 
 	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
 	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
-	primaryWeapon = "object/weapon/ranged/creature/creature_spit_spray_toxicgreen.iff",
+	primaryWeapon = "unarmed",
 	secondaryWeapon = "object/weapon/ranged/creature/creature_spit_spray_toxicgreen.iff",
 	conversationTemplate = "",
-	
+
 	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
 	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
 	primaryAttacks = { {"knockdownattack",""}, {"intimidationattack",""} },
-	secondaryAttacks = { }
+	secondaryAttacks = { {"knockdownattack",""}, {"intimidationattack",""} },
 }
 
 CreatureTemplates:addCreatureTemplate(vrelt_mother, "vrelt_mother")

Puggster/Slave-One

MMOCoreORB/bin/scripts/mobile/corellia/vrelt_mother.lua
@@ -34,13 +34,13 @@ vrelt_mother = Creature:new {
 	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
 	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
 	primaryWeapon = "object/weapon/ranged/creature/creature_spit_spray_toxicgreen.iff",
-	secondaryWeapon = "object/weapon/ranged/creature/creature_spit_spray_toxicgreen.iff",
+	secondaryWeapon = "unarmed",
 	conversationTemplate = "",
-	
+
 	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
 	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
 	primaryAttacks = { {"knockdownattack",""}, {"intimidationattack",""} },
-	secondaryAttacks = { }
+	secondaryAttacks = { {"knockdownattack",""}, {"intimidationattack",""} },
 }
 
 CreatureTemplates:addCreatureTemplate(vrelt_mother, "vrelt_mother")