swgemu/Core3

[fixed] Sunriders Destiny village reward not being generated with randomized condition - Mantis #8721 [added] Test method to receive crystal in createNpc tools menu
Change-Id: I06f3d3120252187ee3b45bf8e8445abc64967430

MMOCoreORB/bin/scripts/screenplays/tools/tools.lua
@@ -6,6 +6,7 @@ StaffTools = {
 		{ "GCW Formation", "openGCWFormationConfig" },
 		{ "Event Active Area", "openEventActiveAreaConfig" },
 		{ "Event AI Control", "openEventAiControlConfig" },
+		{ "Sunriders Crystal Test", "openSunridersCrystalTest" },
 	}
 }
 
@@ -66,6 +67,30 @@ end
 
 function StaffTools.openEventAiControlConfig(pPlayer)
 	EventAiControl:showMainUI(pPlayer)
+end
+
+function StaffTools.openSunridersCrystalTest(pPlayer)
+	if (pPlayer == nil) then
+		return
+	end
+
+	local pInventory = SceneObject(pPlayer):getSlottedObject("inventory")
+
+	if (pInventory ~= nil) then
+		local crystalID = createLoot(pInventory, "color_crystals", 1, true)
+		local pCrystal = getSceneObject(crystalID)
+
+		if (pCrystal == nil) then
+			Logger:logEvent("Crystal is nil. Unable to set Sunriders Crystal Color for Player ID: " .. SceneObject(pPlayer):getObjectID(), LT_ERROR)
+			CreatureObject(pPlayer):sendSystemMessage("There was an error generating your Crystal Reward. Please see Support and screenshot this message.")
+		else
+			local colorCrystal = LuaLightsaberCrystalComponent(pCrystal)
+			colorCrystal:setColor(29)
+			colorCrystal:updateCrystal(29)
+
+			CreatureObject(pPlayer):sendSystemMessage("You receive a Sunriders Destiny Color Crystal")
+		end
+	end
 end
 
 function StaffTools:suiShuttleDropoffCallback(pPlayer, pSui, eventIndex, args)

MMOCoreORB/bin/scripts/screenplays/village/phase3/fs_counterstrike/fs_counterstrike.lua
@@ -324,10 +324,12 @@ function FsCounterStrike:completeQuest(pPlayer, teamComplete)
 	local pInventory = SceneObject(pPlayer):getSlottedObject("inventory")
 
 	if (pInventory ~= nil) then
-		local pCrystal = giveItem(pInventory, "object/tangible/component/weapon/lightsaber/lightsaber_module_force_crystal.iff", -1, true)
+		local crystalID = createLoot(pInventory, "color_crystals", 1, true)
+		local pCrystal = getSceneObject(crystalID)
 
 		if (pCrystal == nil) then
-			CreatureObject(pPlayer):sendSystemMessage("Error: Unable to generate item.")
+			Logger:logEvent("Crystal is nil. Unable to set Sunriders Crystal Color for Player ID: " .. SceneObject(pPlayer):getObjectID(), LT_ERROR)
+			CreatureObject(pPlayer):sendSystemMessage("There was an error generating your Crystal Reward. Please see Support and screenshot this message.")
 		else
 			local colorCrystal = LuaLightsaberCrystalComponent(pCrystal)
 			colorCrystal:setColor(29)

Puggster/Slave-One

MMOCoreORB/bin/scripts/screenplays/tools/tools.lua
@@ -6,6 +6,7 @@ StaffTools = {
 		{ "GCW Formation", "openGCWFormationConfig" },
 		{ "Event Active Area", "openEventActiveAreaConfig" },
 		{ "Event AI Control", "openEventAiControlConfig" },
+		{ "Sunriders Crystal Test", "openSunridersCrystalTest" },
 	}
 }
 
@@ -66,6 +67,30 @@ end
 
 function StaffTools.openEventAiControlConfig(pPlayer)
 	EventAiControl:showMainUI(pPlayer)
+end
+
+function StaffTools.openSunridersCrystalTest(pPlayer)
+	if (pPlayer == nil) then
+		return
+	end
+
+	local pInventory = SceneObject(pPlayer):getSlottedObject("inventory")
+
+	if (pInventory ~= nil) then
+		local crystalID = createLoot(pInventory, "color_crystals", 1, true)
+		local pCrystal = getSceneObject(crystalID)
+
+		if (pCrystal == nil) then
+			Logger:log("Crystal is nil. Unable to set Sunriders Crystal Color for Player ID: " .. SceneObject(pPlayer):getObjectID(), LT_ERROR)
+			CreatureObject(pPlayer):sendSystemMessage("There was an error generating your Crystal Reward. Please see Support and screenshot this message.")
+		else
+			local colorCrystal = LuaLightsaberCrystalComponent(pCrystal)
+			colorCrystal:setColor(29)
+			colorCrystal:updateCrystal(29)
+
+			CreatureObject(pPlayer):sendSystemMessage("You receive a Sunriders Destiny Color Crystal")
+		end
+	end
 end
 
 function StaffTools:suiShuttleDropoffCallback(pPlayer, pSui, eventIndex, args)

MMOCoreORB/bin/scripts/screenplays/village/phase3/fs_counterstrike/fs_counterstrike.lua
@@ -324,10 +324,12 @@ function FsCounterStrike:completeQuest(pPlayer, teamComplete)
 	local pInventory = SceneObject(pPlayer):getSlottedObject("inventory")
 
 	if (pInventory ~= nil) then
-		local pCrystal = giveItem(pInventory, "object/tangible/component/weapon/lightsaber/lightsaber_module_force_crystal.iff", -1, true)
+		local crystalID = createLoot(pInventory, "color_crystals", 1, true)
+		local pCrystal = getSceneObject(crystalID)
 
 		if (pCrystal == nil) then
-			CreatureObject(pPlayer):sendSystemMessage("Error: Unable to generate item.")
+			Logger:log("Crystal is nil. Unable to set Sunriders Crystal Color for Player ID: " .. SceneObject(pPlayer):getObjectID(), LT_ERROR)
+			CreatureObject(pPlayer):sendSystemMessage("There was an error generating your Crystal Reward. Please see Support and screenshot this message.")
 		else
 			local colorCrystal = LuaLightsaberCrystalComponent(pCrystal)
 			colorCrystal:setColor(29)