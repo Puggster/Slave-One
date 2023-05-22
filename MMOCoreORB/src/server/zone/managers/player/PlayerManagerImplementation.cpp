swgemu/Core3

[added] line of sight checks to watch, listen and set performance commands - Mantis #8863
Change-Id: Icc3b65ccfa4595b9cdebdba88595429e94cf190a

MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -3270,9 +3270,14 @@ void PlayerManagerImplementation::startWatch(CreatureObject* creature, uint64 en
 		return;
 	}
 
-	CreatureObject* entertainer = cast<CreatureObject*>( object.get());
+	if (!CollisionManager::checkLineOfSign(object, creature)) {
+		creature->sendSystemMessage("@healing:no_line_of_sight"); // You cannot see your target.
+		return;
+	}
 
-	if (creature == entertainer)
+	CreatureObject* entertainer = cast<CreatureObject*>(object.get());
+
+	if (entertainer == nullptr || creature == entertainer)
 		return;
 
 	Locker clocker(entertainer, creature);
@@ -3330,10 +3335,10 @@ void PlayerManagerImplementation::startListen(CreatureObject* creature, uint64 e
 	if (object == nullptr)
 		return;
 
-	/*if (object->isNonPlayerCreature()) {
-		creature->sendSystemMessage("@performance:dance_watch_npc");
+	if (!CollisionManager::checkLineOfSign(object, creature)) {
+		creature->sendSystemMessage("@healing:no_line_of_sight"); // You cannot see your target.
 		return;
-	}*/
+	}
 
 	// Droid playback handling
 	if (object->isDroidObject()) {

Puggster/Slave-One

MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -3361,9 +3361,14 @@ void PlayerManagerImplementation::startWatch(CreatureObject* creature, uint64 en
 		return;
 	}
 
-	CreatureObject* entertainer = cast<CreatureObject*>( object.get());
+	if (!CollisionManager::checkLineOfSight(object, creature)) {
+		creature->sendSystemMessage("@healing:no_line_of_sight"); // You cannot see your target.
+		return;
+	}
 
-	if (creature == entertainer)
+	CreatureObject* entertainer = cast<CreatureObject*>(object.get());
+
+	if (entertainer == nullptr || creature == entertainer)
 		return;
 
 	Locker clocker(entertainer, creature);
@@ -3421,10 +3426,10 @@ void PlayerManagerImplementation::startListen(CreatureObject* creature, uint64 e
 	if (object == nullptr)
 		return;
 
-	/*if (object->isNonPlayerCreature()) {
-		creature->sendSystemMessage("@performance:dance_watch_npc");
+	if (!CollisionManager::checkLineOfSight(object, creature)) {
+		creature->sendSystemMessage("@healing:no_line_of_sight"); // You cannot see your target.
 		return;
-	}*/
+	}
 
 	// Droid playback handling
 	if (object->isDroidObject()) {