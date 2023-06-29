44a1b27e8d0fec64be89d23942659014dc5d3820
USP <unknownswgplayer@gmail.com>
2023-06-29 14:44:12 -0700

    [Update] Carbines to Medium AP - EE3, Elite, Nyms Slugthrower

--- MMOCoreORB/bin/scripts/object/weapon/ranged/carbine/carbine_nym_slugthrower.lua
+++ MMOCoreORB/bin/scripts/object/weapon/ranged/carbine/carbine_nym_slugthrower.lua
@@ -72,7 +72,7 @@ object_weapon_ranged_carbine_carbine_nym_slugthrower = object_weapon_ranged_carb
 	damageType = KINETIC,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
-	armorPiercing = LIGHT,
+	armorPiercing = MEDIUM,
 
 	-- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
 	-- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
@@ -115,7 +115,7 @@ object_weapon_ranged_carbine_carbine_nym_slugthrower = object_weapon_ranged_carb
 	maxRangeAccuracy = -60,
 
 	minDamage = 160,
-	maxDamage = 300,
+	maxDamage = 450,
 
 	woundsRatio = 16,
 
